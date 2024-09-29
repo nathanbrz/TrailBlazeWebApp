242d73dd55ed7c1b58995e3b4ee3361e30bd46a3
Yousef Fatouraee <fatouraee96@gmail.com>
2024-09-28 23:48:21 -0600

    Add intro section

---
+++ front-end/src/app/components/Dashboard/IntroSection.jsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function IntroSection() {
+  return (
+    <section className="bg-white px-10 py-6">
+      <Container>
+        <Row>
+          <Col>
+            <div className="md:text-left">
+              <span className="text-3xl text-gray-900 mb-4">Hey Mariana -</span>
+              <p className="text-lg text-gray-400 mb-6 inline px-2">
+                here’s what’s happening with your store today
+              </p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="mt-12 text-center">
+              <button className="btn-blaze lg:w-1/4 text-white px-6 py-3 rounded-md hover:bg-red-700 transition-colors">
+                start a new trip!
+              </button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+}
--- front-end/src/app/dashboard/page.jsx
+++ front-end/src/app/dashboard/page.jsx
@@ -1,15 +1,15 @@
 import "../../styles/global_styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchBar from "../components/Dashboard/SearchBar";
-// import IntroSection from "../components/Dashboard/IntroSection";
+import IntroSection from "../components/Dashboard/IntroSection";
 
 export default function Hero() {
   return (
     <div>
       <SearchBar />
-      {/* <div>
+      <div>
         <IntroSection />
-      </div> */}
+      </div>
     </div>
   );
 }
