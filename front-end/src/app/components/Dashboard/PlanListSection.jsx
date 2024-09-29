af8c9de539ce8fe54655eef7bddec2b53266aaa9
Yousef Fatouraee <fatouraee96@gmail.com>
2024-09-29 00:19:09 -0600

    Add plan item list component

---
+++ front-end/src/app/components/Dashboard/PlanListSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlanItem from "./PlanItem";
+export default function PlanListSection() {
+  return (
+    <div>
+      <PlanItem />
+      <PlanItem />
+      <PlanItem />
+      <PlanItem />
+      <PlanItem />
+    </div>
+  );
+}
--- front-end/src/app/dashboard/page.jsx
+++ front-end/src/app/dashboard/page.jsx
@@ -2,6 +2,7 @@ import "../../styles/global_styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchBar from "../components/Dashboard/SearchBar";
 import IntroSection from "../components/Dashboard/IntroSection";
+import PlanListSection from "../components/Dashboard/PlanListSection";
 
 export default function Hero() {
   return (
@@ -9,6 +10,9 @@ export default function Hero() {
       <SearchBar />
       <div>
         <IntroSection />
+      </div>
+      <div>
+        <PlanListSection />
       </div>
     </div>
   );
