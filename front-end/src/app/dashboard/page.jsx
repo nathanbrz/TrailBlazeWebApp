6e8eb0959697dd3a4b3dc11d53d8857ddd547ad0
Yousef Fatouraee <fatouraee96@gmail.com>
2024-09-28 19:30:43 -0600

    Add dashboard index page

---
+++ front-end/src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import "../../styles/global_styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import SearchBar from "../components/Dashboard/SearchBar";
+// import IntroSection from "../components/Dashboard/IntroSection";
+
+export default function Hero() {
+  return (
+    <div>
+      {/* <SearchBar />
+      <IntroSection /> */}
+    </div>
+  );
+}
