d4759cb63fecfa397e271e6655eced650db84ae5
Yousef Fatouraee <fatouraee96@gmail.com>
2024-09-28 23:43:50 -0600

    Add searchform for searchbar

---
+++ front-end/src/app/components/Dashboard/SearchForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+
+const SearchForm = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    if (onSearch) {
+      onSearch(query); // Trigger the search action with the query
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="input-group my-3">
+      <button className="btn btn-light" type="button" onClick={handleSearch}>
+        <i class="bi bi-search"></i>
+      </button>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Type to search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={handleKeyPress} // Allows pressing 'Enter' to search
+      />
+    </div>
+  );
+};
+
+export default SearchForm;
