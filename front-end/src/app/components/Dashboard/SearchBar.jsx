fbb84d1355e29957f7d67816f70e3d6e9b7e5590
Yousef Fatouraee <fatouraee96@gmail.com>
2024-09-28 23:46:37 -0600

    Add searchbar

---
+++ front-end/src/app/components/Dashboard/SearchBar.jsx
@@ -0,0 +1,31 @@
+"use client";
+import React from "react";
+import SearchForm from "./SearchForm";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SearchBar() {
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-content-between h-16">
+          <div className="flex-shrink-0 flex items-center">
+            <Link
+              href="/"
+              style={{ cursor: "pointer" }}
+              className="text-2xl font-bold text-blaze"
+            >
+              TrailBlaze
+            </Link>
+          </div>
+          <div className="flex-shrink-0 flex items-center col-lg-8 col-6">
+            <SearchForm />
+          </div>
+          <div className="flex-shrink-0 flex items-center">
+            <Image src="/images/user-icon.png" width="50" height="50" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
--- front-end/src/app/dashboard/page.jsx
+++ front-end/src/app/dashboard/page.jsx
@@ -1,13 +1,15 @@
 import "../../styles/global_styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import SearchBar from "../components/Dashboard/SearchBar";
+import SearchBar from "../components/Dashboard/SearchBar";
 // import IntroSection from "../components/Dashboard/IntroSection";
 
 export default function Hero() {
   return (
     <div>
-      {/* <SearchBar />
-      <IntroSection /> */}
+      <SearchBar />
+      {/* <div>
+        <IntroSection />
+      </div> */}
     </div>
   );
 }
