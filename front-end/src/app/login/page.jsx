9dbb3b13b673e05072784ea1aba1c027208d234c
NathanD0 <122122847+nathanbrz@users.noreply.github.com>
2024-10-11 15:35:41 -0600

    Trying to redirect away from log in and sign up pages when already logged in

--- front-end/src/app/login/page.jsx
+++ front-end/src/app/login/page.jsx
@@ -1,71 +1,86 @@
-'use client';
-import React, { useState } from 'react';
-import '../../styles/global_styles.css';
-import { doSignInUserWithEmailAndPassword  } from '../firebase/auth';
-import { useRouter } from 'next/navigation';
-import useAuth from '../hooks/useAuth'; 
-
-
+"use client";
+import React, { useState } from "react";
+import "../../styles/global_styles.css";
+import { doSignInUserWithEmailAndPassword } from "../firebase/auth";
+import { useRouter } from "next/navigation";
+import useAuth from "../hooks/useAuth";
 
 const Login = () => {
-
   // Checks if user is already logged in
   useAuth();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const router = useRouter();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     // Handling authentication through firebase
     try {
-      const userCredential = await doSignInUserWithEmailAndPassword(email, password);
+      const userCredential = await doSignInUserWithEmailAndPassword(
+        email,
+        password
+      );
       console.log("User logged in!");
 
       // Get the user's token after successful login
       const token = await userCredential.user.getIdToken();
 
       // Send the token to the backend for verification
-      const response = await fetch(`http://localhost:${process.env.NEXT_PUBLIC_BACK_END_PORT}/api/firebase/session`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({ token }), // Send the token to the backend
-      });
- 
-       // Check for errors in the response
-       if (!response.ok) {
-         throw new Error("Failed to verify token");
-       }
- 
-       // Get the response data
-       const data = await response.json();
-       console.log('Session established:', data);
+      const response = await fetch(
+        `http://localhost:${process.env.NEXT_PUBLIC_BACK_END_PORT}/api/firebase/session`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ token }), // Send the token to the backend
+        }
+      );
+
+      // Check for errors in the response
+      if (!response.ok) {
+        throw new Error("Failed to verify token");
+      }
+
+      // Get the response data
+      const data = await response.json();
+      console.log("Session established:", data);
 
       // Saving the token and userID in local storage
-      localStorage.setItem('token', String(token));
-      localStorage.setItem('uuid', data.uid);
+      localStorage.setItem("token", String(token));
+      localStorage.setItem("uuid", data.uid);
 
-      router.push('../dashboard');
+      router.push("/dashboard");
     } catch (error) {
       setErrorMessage(error.message);
       console.log("Login in failed");
     }
-    console.log('Login submitted:', { email, password });
+    console.log("Login submitted:", { email, password });
+  };
+
+  const handdleSignUp = () => {
+    router.push("/signup");
   };
 
   return (
-    <div className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
-      <div className="card p-12 shadow-lg" style={{ width: '100%', maxWidth: '400px' }}>
+    <div
+      className="d-flex align-items-center justify-content-center"
+      style={{ height: "100vh" }}
+    >
+      <div
+        className="card p-12 shadow-lg"
+        style={{ width: "100%", maxWidth: "400px" }}
+      >
         <h2 className="text-center mb-4">Login</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email address</label>
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
             <input
               type="email"
               className="form-control"
@@ -77,7 +92,9 @@ const Login = () => {
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="password" className="form-label">Password</label>
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
             <input
               type="password"
               className="form-control"
@@ -88,8 +105,20 @@ const Login = () => {
               required
             />
           </div>
-          <button type="submit" className="btn-blaze text-white px-6 py-3 w-100 rounded-md hover:bg-red-700 transition-colors">Login</button>
+          <button
+            type="submit"
+            className="btn-blaze text-white px-6 py-3 w-100 rounded-md hover:bg-red-700 transition-colors"
+          >
+            Login
+          </button>
         </form>
+        <button
+          type="submit"
+          className="bg-zinc-200 my-2 text-black px-6 py-3 w-100 rounded-md hover:bg-zinc-400 transition-colors"
+          onClick={handdleSignUp}
+        >
+          Sign up
+        </button>
       </div>
     </div>
   );
