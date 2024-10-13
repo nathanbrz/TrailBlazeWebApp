07480782b9fcb1e1668b9d3e3cd3517d159978cd
Yousef Fatouraee <fatouraee96@gmail.com>
2024-10-13 09:39:04 -0600

    Refactor dashboard index page to use useApi hook

--- front-end/src/app/dashboard/[useId]/page.jsx
+++ front-end/src/app/dashboard/[useId]/page.jsx
@@ -7,51 +7,42 @@ import PlanListSection from "../../components/Dashboard/PlanListSection";
 import Footer from "../../components/Footer";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { useApi } from "../../hooks/useApi";
 
 export default function Hero() {
   const router = useRouter();
   const [user, setUser] = useState(null);
+  const [alert, setAlert] = useState({
+    show: false,
+    message: "",
+    variant: "",
+  });
 
-  const URL = process.env.NEXT_PUBLIC_BACK_END_URL || 'http://localhost';
-  const PORT = process.env.NEXT_PUBLIC_BACK_END_PORT || '4000'
+  const firebaseUID = localStorage.getItem("uuid");
 
+  // Fetch user data using useApi hook
+  const { data, loading, error } = useApi(`api/users/${firebaseUID}`, "GET");
 
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const token = localStorage.getItem("token");
-        const firebaseUID = localStorage.getItem("uuid"); // Retrieve firebaseUID
+    if (!firebaseUID) {
+      router.push("/login");
+      setAlert({
+        show: true,
+        message: "No firebaseUID found",
+        variant: "danger",
+      });
+    }
+  }, [firebaseUID, router]);
 
-        if (!firebaseUID) {
-          router.push("/login");
+  // Check for error from useApi and set the alert accordingly
+  useEffect(() => {
+    if (error) {
+      console.error("Error fetching user data:", error);
+    } else if (data && data.user) {
+      setUser(data.user);
+    }
+  }, [data, error]);
 
-          throw new Error("No firebaseUID found");
-        }
-
-        const response = await fetch(
-          `${URL}:${PORT}/api/users/${firebaseUID}`,
-          {
-            method: "GET",
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch user data");
-        }
-
-        const userData = await response.json();
-        setUser(userData.user);
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
-
-    fetchUserData();
-  }, []);
-  console.log(user);
   return (
     <div>
       <div className="page-container">
