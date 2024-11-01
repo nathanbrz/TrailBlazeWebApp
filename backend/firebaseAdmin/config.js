349dddf2e0ba84be257ef9d235756acd0ae73e91
Destin Saba <129232125+destinsaba@users.noreply.github.com>
2024-11-01 15:37:12 -0600

    Removing dependency on dotenv for deployment

--- backend/firebaseAdmin/config.js
+++ backend/firebaseAdmin/config.js
@@ -1,7 +1,10 @@
 const { initializeApp } = require('firebase-admin/app');
 const admin = require('firebase-admin');
 const credentials = require('./firebaseCredentials.json')
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 
 // Initalizing Web App Instance
 admin.initializeApp({
