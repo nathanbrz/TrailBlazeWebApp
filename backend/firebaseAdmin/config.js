4e6052657b389971a465c9dfe7edaaeb7b73ac32
Destin Saba <129232125+destinsaba@users.noreply.github.com>
2024-11-01 18:09:15 -0600

    Testing JSON

--- backend/firebaseAdmin/config.js
+++ backend/firebaseAdmin/config.js
@@ -2,12 +2,11 @@ const { initializeApp } = require('firebase-admin/app');
 const admin = require('firebase-admin');
 
 let credentials;
-
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
-  credentials = require('./firebaseCredentials.json')
+  credentials = require('./firebaseCredentials.json');
 } else {
-  credentials = require('/etc/secrets/firebaseCredentials.json')
+  credentials = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 }
 
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const cors = require('cors');
 
+
 const mongoose = require('mongoose')
 const uri = process.env.MONGODB_URI
 const port = process.env.PORT || 4000 //port = 4000 unless specified in .env
@@ -25,16 +26,25 @@ const firebaseRoutes = require('./routes/firebaseRoutes')
 // Create express app
 const app = express()
 
-// Middleware Setup
-const corsOptions = {
-    origin: `${FRONTENDURL}:${FRONTENDPORT}`,
-    methods: ["GET", "POST", "PUT", "DELETE"], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-};
+const allowedOrigins = [
+    'https://trailblazeapp.vercel.app/', // Production frontend on Vercel
+    `${FRONTENDURL}:${FRONTENDPORT}`,    // Local development frontend
+  ];
 
-// Server and Middleware setup
-app.use(cors(corsOptions));
+// Middleware Setup
+app.use(cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true); // Origin is allowed
+      } else {
+        callback(new Error('Not allowed by CORS')); // Origin is not allowed
+      }
+    }
+  }));
+
+
 app.use(express.json());
 
 app.use((req, res, next) => {
