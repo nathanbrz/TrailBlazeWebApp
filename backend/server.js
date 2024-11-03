b4a5b102a24b882c9b89ff4c6c61fd898f96f4ce
ranaelsa <relsadig@ualberta.ca>
2024-11-03 09:35:53 -0700

    configured server.js to allow for testing

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,9 @@ const mongoose = require("mongoose");
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 4000; // Default port is 4000 if not specified
 
-const User = require("./dbmodels/user");
-const Trip = require("./dbmodels/trip");
-
 // Import route modules
 const tripRoutes = require("./routes/tripRoutes");
 const userRoutes = require("./routes/userRoutes");
-const promptRoutes = require("./routes/promptRoutes");
 const firebaseRoutes = require("./routes/firebaseRoutes"); // Authentication
 
 // Frontend configuration (production and development)
@@ -79,38 +75,25 @@ app.use("/api/trips", tripRoutes); // Routes for trip-related endpoints
 app.use("/api/users", userRoutes); // Routes for user-related endpoints
 app.use("/api/firebase", firebaseRoutes); // Routes for Firebase authentication-related endpoints
 
-// Export the app for testing
-module.exports = app;
-
-// Connect to the database and start the server if not in a test environment
-if (process.env.NODE_ENV !== "test") {
-  async function connect() {
-    try {
-      await mongoose.connect(uri);
-      console.log("Connected to MongoDB!");
-      app.listen(port, () => {
-        console.log("Server is listening on port " + port);
-      });
-    } catch (err) {
-      console.error(err);
-    }
+// Function to connect to MongoDB and start the server
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB!");
+  } catch (err) {
+    console.error(err);
+  } finally {
+    // Start listening for incoming requests on the specified port
+    app.listen(port, () => {
+      console.log("Server is listening on port" + port);
+    });
   }
 }
-connect(); // Call the connect function to start the server
 
-// Here is some example code showing how to create and query MongoDB models.
-// try {
-//     // const testuser = new User({
-//     //     userID: new mongoose.Types.ObjectId(),
-//     //     username: "cole_C",
-//     //     email: "fake.email@gmail.com"
-//     // })
-//     // testuser.save()
-// } catch (err) {
-//     console.error(err)
-// } finally {
-//     User.find({username: "cole_C"})
-//         .then((res) => {
-//             console.log(res)
-//         })
-// }
+// Export the app for testing purposes
+module.exports = app;
+
+// Call the connect function if not in a test environment
+if (process.env.NODE_ENV !== "test") {
+  connect();
+}
