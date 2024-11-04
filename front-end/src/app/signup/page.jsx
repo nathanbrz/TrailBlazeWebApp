e6c0ad6787d333ec7ca31d7e7abe797198cdad3b
Riley <koppangdev@gmail.com>
2024-11-03 20:09:49 -0700

    Fixed CSS issue on login and signup page

--- front-end/src/app/signup/page.jsx
+++ front-end/src/app/signup/page.jsx
@@ -7,6 +7,8 @@ import { useRouter } from "next/navigation";
 import useAuth from "../hooks/useAuth";
 import { useApi } from "../hooks/useApi";
 import MessageAlert from "../components/MessageAlert";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Signup = () => {
   // Checks if user is already logged in
