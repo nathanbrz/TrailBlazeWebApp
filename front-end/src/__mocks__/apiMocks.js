f17bbf30568b6f23b1b0212252221a631e6707bf
Riley <koppangdev@gmail.com>
2024-11-04 13:50:07 -0700

    Added Login Tests

--- front-end/src/__mocks__/apiMocks.js
+++ front-end/src/__mocks__/apiMocks.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
-import { doCreateUserWithEmailAndPassword } from '../app/firebase/auth.js';
+import { doCreateUserWithEmailAndPassword, doSignInUserWithEmailAndPassword } from '../app/firebase/auth.js';
 import { useApi } from '../app/hooks/useApi.js';
+import { useRouter } from 'next/router.js';
 
+// Creating a mock function for the useAPI 
 jest.mock("../app/hooks/useApi", () => ({
     useApi: jest.fn(() => ({
         fetchData: jest.fn(),
@@ -11,12 +13,25 @@ jest.mock("../app/hooks/useApi", () => ({
     })),
 }));
 
+// Mock function for firebase API functions
 jest.mock('../app/firebase/auth', () => ({
+
+    // Mock function for doCreateUserWithEmail
     doCreateUserWithEmailAndPassword: jest.fn().mockResolvedValue({
         user: {
             getIdToken: jest.fn().mockResolvedValue('mocked-token'),
             uid: 'mocked-uid',
         },
+    }),
+
+    // Mock function for signInWithEmailAndPassword
+    doSignInUserWithEmailAndPassword: jest.fn().mockResolvedValue({
+        user: {
+            uid: 'mocked-uid',
+            email: 'user@example.com',
+            displayName: 'Mock User',
+            getIdToken: jest.fn().mockResolvedValue('mocked-token')
+        }
     }),
     initializeApp: jest.fn(),
     getAuth: jest.fn(() => ({
@@ -24,8 +39,19 @@ jest.mock('../app/firebase/auth', () => ({
     })),
 }));
 
+// Mock function for the navigation call
 jest.mock('next/navigation', () => ({
-    useRouter: () => ({
+    useRouter: jest.fn(() => ({
         push: jest.fn(),
-    }),
+    })),
 }));
+
+// Mock function for the navigation call
+jest.mock('next/router', () => ({
+    useRouter: jest.fn(() => ({
+        push: jest.fn(),
+    })),
+}));
+
+
+
