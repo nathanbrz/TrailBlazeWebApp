7f3c8a57ad7592e3c73b068ffa786563da3f5073
NathanD0 <122122847+nathanbrz@users.noreply.github.com>
2024-11-06 14:16:31 -0700

    Added a back to dashboard button from itinerary

--- front-end/src/app/itinerary/page.jsx
+++ front-end/src/app/itinerary/page.jsx
@@ -6,12 +6,16 @@ import React from "react";
 import TimelineView from "../components/Itinerary/TimelineView";
 import GenericNav from "../components/GenericNav";
 import Footer from "../components/Footer";
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter  } from "next/navigation";
 import withAuth from "../components/withAuth"; // Import the HOC
 import { Suspense } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css"; // Import Bootstrap Icons
 
 function ItineraryPage() {
   const searchParams = useSearchParams(); // Hook to retrieve query parameters
+  const router = useRouter(); // Initialize router here
+  const uid = localStorage.getItem("uuid");
+
   const encodedData = searchParams.get("itinerary"); // Get the "itinerary" parameter
   const itinerary = encodedData
     ? JSON.parse(decodeURIComponent(encodedData)) // Decode and parse the itinerary data
@@ -23,6 +27,14 @@ function ItineraryPage() {
         <div className="page-container">
           <div className="content">
             <GenericNav />
+            <button
+              onClick={() => router.push(`/dashboard/${uid}`)}
+              className="btn-blaze text-white m-6 px-3 py-2 rounded-md hover:bg-red-700 transition-colors cursor-pointer"
+            >
+              <i className="bi bi-arrow-left me-2"></i>{" "}
+              {/* Bootstrap Icon for Back Arrow */}
+              Dashboard
+            </button>
             <TimelineView itinerary={itinerary} />
           </div>
           <Footer />
