d325c1fe8cd6353e75e869fd1bee255d3ee0a248
Riley <koppangdev@gmail.com>
2024-11-06 18:56:42 -0700

    Finished more front end testing

--- front-end/src/__mocks__/apiMocks.js
+++ front-end/src/__mocks__/apiMocks.js
@@ -1,80 +1,106 @@
-import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
-import { doCreateUserWithEmailAndPassword, doSignInUserWithEmailAndPassword } from '../app/firebase/auth';
-import { useApi } from '../app/hooks/useApi.js';
-import { useRouter } from 'next/router.js';
-import { onIdTokenChanged } from 'firebase/auth';
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import {
+  doCreateUserWithEmailAndPassword,
+  doSignInUserWithEmailAndPassword,
+} from "../app/firebase/auth";
+import { useApi } from "../app/hooks/useApi.js";
+import { useRouter } from "next/router.js";
+import {
+  onIdTokenChanged,
+  doUpdateEmail,
+  doNameChange,
+  doSignOut,
+  doPasswordChange,
+} from "firebase/auth";
+import withAuth from "@/app/components/withAuth";
 
-
-// Creating a mock function for the useAPI 
+// Creating a mock function for the useAPI
 jest.mock("../app/hooks/useApi", () => ({
-    useApi: jest.fn(() => ({
-        fetchData: jest.fn(),
-        error: null,
-        responseStatus: 200, 
-    })),
+  useApi: jest.fn(() => ({
+    fetchData: jest.fn(),
+    error: null,
+    responseStatus: 200,
+  })),
 }));
 
 // Mock function for the firebase config
-jest.mock('../app/firebase/config', () => ({
-    firebaseConfig: {
-        apiKey: "mocked-api-key",
-        authDomain: "mocked-auth-domain",
-        projectId: "mocked-project-id",
-        storageBucket: "mocked-storage-bucket",
-        messagingSenderId: "mocked-messaging-sender-id",
-        appId: "mocked-app-id"
-    }
+jest.mock("../app/firebase/config", () => ({
+  firebaseConfig: {
+    apiKey: "mocked-api-key",
+    authDomain: "mocked-auth-domain",
+    projectId: "mocked-project-id",
+    storageBucket: "mocked-storage-bucket",
+    messagingSenderId: "mocked-messaging-sender-id",
+    appId: "mocked-app-id",
+  },
 }));
-
-
-
 
 // Mock function for firebase API functions
-jest.mock('../app/firebase/auth', () => ({
+jest.mock("../app/firebase/auth", () => ({
+  // Mock function for doCreateUserWithEmail
+  doCreateUserWithEmailAndPassword: jest.fn().mockResolvedValue({
+    user: {
+      getIdToken: jest.fn().mockResolvedValue("mocked-token"),
+      uid: "mocked-uid",
+    },
+  }),
 
-    // Mock function for doCreateUserWithEmail
-    doCreateUserWithEmailAndPassword: jest.fn().mockResolvedValue({
-        user: {
-            getIdToken: jest.fn().mockResolvedValue('mocked-token'),
-            uid: 'mocked-uid',
-        },
-    }),
+  onIdTokenChanged: jest.fn((auth, callback) => {
+    // Call the callback immediately with a mocked user object
+    callback({ getIdToken: jest.fn(() => Promise.resolve("mocked-token")) });
+    return jest.fn(); // Return a mock unsubscribe function
+  }),
 
-    onIdTokenChanged: jest.fn((auth, callback) => {
-        // Call the callback immediately with a mocked user object
-        callback({ getIdToken: jest.fn(() => Promise.resolve('mocked-token')) });
-        return jest.fn(); // Return a mock unsubscribe function
-      }),
+  // Mock function for signInWithEmailAndPassword
+  doSignInUserWithEmailAndPassword: jest.fn().mockResolvedValue({
+    user: {
+      uid: "mocked-uid",
+      email: "user@example.com",
+      displayName: "Mock User",
+      getIdToken: jest.fn().mockResolvedValue("mocked-token"),
+    },
+  }),
 
-    // Mock function for signInWithEmailAndPassword
-    doSignInUserWithEmailAndPassword: jest.fn().mockResolvedValue({
-        user: {
-            uid: 'mocked-uid',
-            email: 'user@example.com',
-            displayName: 'Mock User',
-            getIdToken: jest.fn().mockResolvedValue('mocked-token')
-        }
-    }),
-    initializeApp: jest.fn(),
-    getAuth: jest.fn(() => ({
-        signInWithEmailAndPassword: jest.fn(),
-    })),
+  // Mock function for settings page password change
+  doPasswordChange: jest.fn(),
+
+  // Mock function for settings page doSignout
+  doSignOut: jest.fn(),
+
+  // Mock function for settings page deleting a user
+  doDeleteUser: jest.fn(),
+
+  // Mock function for settings page changing users email
+  doUpdateEmail: jest.fn(),
+
+  initializeApp: jest.fn(),
+  getAuth: jest.fn(() => ({
+    signInWithEmailAndPassword: jest.fn(),
+  })),
 }));
 
 // Mock function for the navigation call
-jest.mock('next/navigation', () => ({
-    useRouter: jest.fn(() => ({
-        push: jest.fn(),
-    })),
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(() => ({
+    push: jest.fn(),
+  })),
 }));
+
+// Mocking the GenericNav for the settings page
+jest.mock("../app/components/GenericNav", () => {
+  return {
+    __esModule: true,
+    default: jest.fn(() => <div>Mocked GenericNav</div>),
+  };
+});
 
 // Mock function for the navigation call
-jest.mock('next/router', () => ({
-    useRouter: jest.fn(() => ({
-        push: jest.fn(),
-    })),
+jest.mock("next/router", () => ({
+  useRouter: jest.fn(() => ({
+    query: { id: 123 },
+    push: jest.fn(),
+  })),
 }));
 
-
-
+jest.mock("@/app/components/withAuth", () => jest.fn((Component) => Component));
--- front-end/src/app/firebase/auth.js
+++ front-end/src/app/firebase/auth.js
@@ -12,105 +12,110 @@ import { updateProfile } from "firebase/auth";
 
 // Creates user and stores information
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
-    try {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        // New user created successfully
-        return userCredential;
-    } catch (error) {
-        // Error creating new user
-        console.error("Error creating new user", error);
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    // New user created successfully
+    return userCredential;
+  } catch (error) {
+    // Error creating new user
+    console.error("Error creating new user", error);
 
-        // Output specific error message
-        let errorMessage;
-        switch (error.code) {
-            case 'auth/email-already-in-use':
-                errorMessage = "This email address is already in use.";
-                break;
-            case 'auth/weak-password':
-                errorMessage = "Password should be at least 6 characters.";
-                break;
-            case 'auth/invalid-email':
-                errorMessage = "Please enter a valid email address.";
-                break;
-            default:
-                errorMessage = "Failed to create user. Please try again.";
-                break;
-        }
-        throw new Error(errorMessage);
+    // Output specific error message
+    let errorMessage;
+    switch (error.code) {
+      case "auth/email-already-in-use":
+        errorMessage = "This email address is already in use.";
+        break;
+      case "auth/weak-password":
+        errorMessage = "Password should be at least 6 characters.";
+        break;
+      case "auth/invalid-email":
+        errorMessage = "Please enter a valid email address.";
+        break;
+      default:
+        errorMessage = "Failed to create user. Please try again.";
+        break;
     }
+    throw new Error(errorMessage);
+  }
 };
-
 
 /*
-* Signing in an existing user
-* Returns a userCrediential object
-    * Contains user instance which can access uid, email, displayName, photoURL, getIdToken
-*/
+ * Signing in an existing user
+ * Returns a userCrediential object
+ * Contains user instance which can access uid, email, displayName, photoURL, getIdToken
+ */
 export const doSignInUserWithEmailAndPassword = async (email, password) => {
-    try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        // Sign in worked
-        return userCredential;
-    } catch (error) {
-        // Error signing in user
-        console.error("Error signing in user:", error);
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    // Sign in worked
+    return userCredential;
+  } catch (error) {
+    // Error signing in user
+    console.error("Error signing in user:", error);
 
-        // Output specific error message
-        let errorMessage;
-        switch (error.code) {
-            case 'auth/user-not-found':
-                errorMessage = "No user found with this email.";
-                break;
-            case 'auth/wrong-password':
-                errorMessage = "Incorrect password. Please try again.";
-                break;
-            case 'auth/invalid-email':
-                errorMessage = "Please enter a valid email address.";
-                break;
-            default:
-                errorMessage = "Failed to sign in. Please try again.";
-                break;
-        }
-        throw new Error(errorMessage); // Re-throw the error with a custom message
+    // Output specific error message
+    let errorMessage;
+    switch (error.code) {
+      case "auth/user-not-found":
+        errorMessage = "No user found with this email.";
+        break;
+      case "auth/wrong-password":
+        errorMessage = "Incorrect password. Please try again.";
+        break;
+      case "auth/invalid-email":
+        errorMessage = "Please enter a valid email address.";
+        break;
+      default:
+        errorMessage = "Failed to sign in. Please try again.";
+        break;
     }
+    throw new Error(errorMessage); // Re-throw the error with a custom message
+  }
 };
-
 
 // User signs in with Google method
 export const doSignInWithGoogle = async () => {
-    const provider = new GoogleAuthProvider();
-    try {
-        const result = await signInWithPopup(auth, provider);
-        // Successfully signed in with Google
-        return result;        
-    } catch (error) {
-        // Google sign in failed
-        console.error("Error signing in with Google:", error);
-        throw new Error("Failed to sign in with Google. Please try again.");
-    }
+  const provider = new GoogleAuthProvider();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    // Successfully signed in with Google
+    return result;
+  } catch (error) {
+    // Google sign in failed
+    console.error("Error signing in with Google:", error);
+    throw new Error("Failed to sign in with Google. Please try again.");
+  }
 };
 
 // Signing user out
 export const doSignOut = async () => {
-    try {
-        await signOut(auth);
-        console.log("User successfully signed out");
-    } catch (error) {
-        console.log("Error signing out: " , error);
-        throw new Error("Failed to sign out. Please try again");
-    }
+  try {
+    await signOut(auth);
+    console.log("User successfully signed out");
+  } catch (error) {
+    console.log("Error signing out: ", error);
+    throw new Error("Failed to sign out. Please try again");
+  }
 };
-
 
 // Password reset for user
 export const doPasswordResetEmail = async (email) => {
-    try {
-        const result = await sendPasswordResetEmail(auth, email);
-        alert("Password Reset has been sent. Please check your inbox");
-        return result
-    } catch (error) {
-        console.error(error.message);
-    }
+  try {
+    const result = await sendPasswordResetEmail(auth, email);
+    alert("Password Reset has been sent. Please check your inbox");
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 /* 
@@ -118,18 +123,18 @@ export const doPasswordResetEmail = async (email) => {
     User has to be currently logged in
 */
 export const doPasswordChange = async (newPassword) => {
-    try {
-        const user = auth.currentUser;
-        if (user) {
-            await updatePassword(user, newPassword);
-            console.log("Password updated successfully");
-        } else {
-            throw new Error("User is not currently logged in");
-        }
-    } catch (error) {
-        console.log("Error changing password: ", error.message);
-        throw new Error("Error changing password. Please try again");
+  try {
+    const user = auth.currentUser;
+    if (user) {
+      await updatePassword(user, newPassword);
+      console.log("Password updated successfully");
+    } else {
+      throw new Error("User is not currently logged in");
     }
+  } catch (error) {
+    console.log("Error changing password: ", error.message);
+    throw new Error("Error changing password. Please try again");
+  }
 };
 
 /* 
@@ -137,18 +142,18 @@ export const doPasswordChange = async (newPassword) => {
     User has to be currently logged in
 */
 export const doDeleteUser = async () => {
-    try {
-        const user = auth.currentUser;
-        if (user) {
-            await deleteUser(user);
-            console.log("User deleted successfully");
-        } else {
-            throw new Error("User is not currently logged in");
-        }
-    } catch (error) {
-        console.error("Error deleting user:", error.message);
-        throw new Error("Error deleting user. Please try again");
+  try {
+    const user = auth.currentUser;
+    if (user) {
+      await deleteUser(user);
+      console.log("User deleted successfully");
+    } else {
+      throw new Error("User is not currently logged in");
     }
+  } catch (error) {
+    console.error("Error deleting user:", error.message);
+    throw new Error("Error deleting user. Please try again");
+  }
 };
 
 /* 
@@ -156,18 +161,18 @@ export const doDeleteUser = async () => {
     User has to be currently logged in
 */
 export const doUpdateEmail = async (newEmail) => {
-    try {
-        const user = auth.currentUser;
-        if (user) {
-            await updateEmail(user, newEmail);
-            console.log("Email updated successfully");
-        } else {
-            throw new Error("User is not currently logged in");
-        }
-    } catch (error) {
-        console.error("Error updating email:", error.message);
-        throw new Error("Error updating email. Please try again");
+  try {
+    const user = auth.currentUser;
+    if (user) {
+      await updateEmail(user, newEmail);
+      console.log("Email updated successfully");
+    } else {
+      throw new Error("User is not currently logged in");
     }
+  } catch (error) {
+    console.error("Error updating email:", error.message);
+    throw new Error("Error updating email. Please try again");
+  }
 };
 
 /* 
@@ -175,16 +180,16 @@ export const doUpdateEmail = async (newEmail) => {
     User has to be currently logged in
 */
 export const doUpdateName = async (newName) => {
-    try {
-        const user = auth.currentUser;
-        if (user) {
-            await updateProfile(user, { displayName: newName });
-            console.log("Name updated successfully");
-        } else {
-            throw new Error("User is not currently logged in");
-        }
-    } catch (error) {
-        console.error("Error updating name:", error.message);
-        throw new Error("Error updating name. Please try again");
+  try {
+    const user = auth.currentUser;
+    if (user) {
+      await updateProfile(user, { displayName: newName });
+      console.log("Name updated successfully");
+    } else {
+      throw new Error("User is not currently logged in");
     }
+  } catch (error) {
+    console.error("Error updating name:", error.message);
+    throw new Error("Error updating name. Please try again");
+  }
 };
